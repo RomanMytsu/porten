7c3cd60bb02736681f8c4b0229049e5a72692226
Roman <romanmytsu@gmail.com>
2025-08-14 14:12:50 +0300

    add collection section

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,23 +1,23 @@
-import Swiper from "swiper";
-import "swiper/css";
+// import Swiper from "swiper";
+// import "swiper/css";
 
-const swiper = new Swiper(".swiper", {
-  slidesPerView: 1,
-  spaceBetween: 20,
-  breakpoints: {
-    768: {
-      slidesPerView: 2,
-      spaceBetween: 30,
-    },
-    1024: {
-      slidesPerView: 3,
-      spaceBetween: 35,
-    },
-    1440: {
-      spaceBetween: 40,
-    },
-    1920: {
-      spaceBetween: 50,
-    },
-  },
-});
+// const swiper = new Swiper(".swiper", {
+//   slidesPerView: 1,
+//   spaceBetween: 20,
+//   breakpoints: {
+//     768: {
+//       slidesPerView: 2,
+//       spaceBetween: 30,
+//     },
+//     1024: {
+//       slidesPerView: 3,
+//       spaceBetween: 35,
+//     },
+//     1440: {
+//       spaceBetween: 40,
+//     },
+//     1920: {
+//       spaceBetween: 50,
+//     },
+//   },
+// });
