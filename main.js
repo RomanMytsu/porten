a65be8453c02d6ed3c50c29b87e6cb4c15c20b5b
Roman <romanmytsu@gmail.com>
2025-08-17 15:29:33 +0300

    add

---
+++ main.js
@@ -0,0 +1,5 @@
+import "./src/style/reset.css";
+import "./src/style/style.css";
+import "./src/style/media.css";
+import "./src/js/mob-menu";
+import "./src/js/swiper";
--- src/js/main.js
+++
@@ -1,5 +0,0 @@
-import "../style/reset.css";
-import "../style/style.css";
-import "../style/media.css";
-import "./mob-menu";
-import "./swiper";
